5d73faed7a88e2bd5a5a777665754d4344a9729d
MisterToby <brithom.parker@gmail.com>
2014-03-10 18:30:26 -0400

    Added a checkout button and page

--- cart.php
+++ cart.php
@@ -5,19 +5,30 @@ include "header.php";
 ?>
 <div class="row">
 
-    <div class="col-sm-8 col-sm-offset-1">
+    <div class="col-sm-10 col-sm-offset-1">
 	
         <h2>Cart</h2>
 		
 		<table class="table">
+            <head>
+                <th></th>
+                <th>Item</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Sub Total</th>
+                <th></th>
+                
+            </head>
         <?php
-        
+            $subTotal = 0;
+            $total = 0;
 			// initialize cart manager -nm
 			$CART_MGR = CartManager::getInstance();
 			
 			// for each item in the cart -nm
 			foreach( $CART_MGR->getItems() as $index => $item ){
-				
+				$subTotal = 0;
 				//print_r( $item );
 				// print item to screen -nm
 				echo '<tr>' . "\n";
@@ -25,15 +36,29 @@ include "header.php";
 				echo '<td>' . $item['name'] . '</td>' . "\n";
 				echo '<td>' . $item['description'] . '</td>' . "\n";
 				echo '<td>$' . $item['price'] . '</td>' . "\n";
-                echo '<td> Qty:' . $item['quantity'] . '</td>' . "\n";
-				
+                echo '<td> x' . $item['quantity'] . '</td>' . "\n";
+                $subTotal += ($item['price'] * $item['quantity']);
+                $total += ($item['price'] * $item['quantity']);
+                echo '<td>$' . $subTotal . '</td>';
+				//echo $total;
+                
 				// TODO: action="cart_remove_item.php"
 				echo '<td> <form id="removeCartForm" method="POST" action=""> <button id="remove" type="submit" class="btn btn-default">Remove</button>
 					<input type="hidden" name="index" value="' . $index . '"/> </form>' . "\n";
 				echo '</tr>' . "\n";
+                
 			}
         ?>
         </table>
+        <div class="col-sm-4 col-sm-offset-8" align="right">
+            <form action="checkout.php" method="POST" id="checkout">
+            <?php echo "Total: $" . $total . "\n "; ?>
+            
+                <input type="hidden" name="total" value="<?php echo $total; ?>"/>
+                <button class="btn btn-default" type="submit" name="submit" value="Checkout">Check out</button>
+            </form>
+        
+        </div>
     </div>
 </div>
 
