5a73166888a88348bcd485de5ae9fa9e63d17cb1
uptownjimmy <lljj@mac.com>
2014-03-16 18:44:55 -0400

    Checkout enhancements

--- checkout.php
+++ checkout.php
@@ -120,7 +120,6 @@ $total = 0;
     </div>
     
     <div class="col-sm-4 col-sm-offset-1">
-<<<<<<< HEAD
         <form class="form-horizontal" method="POST" action="">
             <h2>Credit Card Information</h2>
             <div class="form-group">
@@ -188,19 +187,75 @@ $total = 0;
                             <input type="text" class="form-control" name="city" value="<?php echo $city; ?>"/>
                         </div>
                     </div>
+
+
                     <div class="form-group">
                         <label for="" class="control-label col-xs-4">State</label>
                         <div class="col-xs-8">
-                            <input type="text" class="form-control" name="state" value="<?php echo $state; ?>"/>
+                            <select name="state"> 
+                            <option value="" selected="selected">Select a State</option> 
+                            <option value="AL">Alabama</option> 
+                            <option value="AK">Alaska</option> 
+                            <option value="AZ">Arizona</option> 
+                            <option value="AR">Arkansas</option> 
+                            <option value="CA">California</option> 
+                            <option value="CO">Colorado</option> 
+                            <option value="CT">Connecticut</option> 
+                            <option value="DE">Delaware</option> 
+                            <option value="DC">District Of Columbia</option> 
+                            <option value="FL">Florida</option> 
+                            <option value="GA">Georgia</option> 
+                            <option value="HI">Hawaii</option> 
+                            <option value="ID">Idaho</option> 
+                            <option value="IL">Illinois</option> 
+                            <option value="IN">Indiana</option> 
+                            <option value="IA">Iowa</option> 
+                            <option value="KS">Kansas</option> 
+                            <option value="KY">Kentucky</option> 
+                            <option value="LA">Louisiana</option> 
+                            <option value="ME">Maine</option> 
+                            <option value="MD">Maryland</option> 
+                            <option value="MA">Massachusetts</option> 
+                            <option value="MI">Michigan</option> 
+                            <option value="MN">Minnesota</option> 
+                            <option value="MS">Mississippi</option> 
+                            <option value="MO">Missouri</option> 
+                            <option value="MT">Montana</option> 
+                            <option value="NE">Nebraska</option> 
+                            <option value="NV">Nevada</option> 
+                            <option value="NH">New Hampshire</option> 
+                            <option value="NJ">New Jersey</option> 
+                            <option value="NM">New Mexico</option> 
+                            <option value="NY">New York</option> 
+                            <option value="NC">North Carolina</option> 
+                            <option value="ND">North Dakota</option> 
+                            <option value="OH">Ohio</option> 
+                            <option value="OK">Oklahoma</option> 
+                            <option value="OR">Oregon</option> 
+                            <option value="PA">Pennsylvania</option> 
+                            <option value="RI">Rhode Island</option> 
+                            <option value="SC">South Carolina</option> 
+                            <option value="SD">South Dakota</option> 
+                            <option value="TN">Tennessee</option> 
+                            <option value="TX">Texas</option> 
+                            <option value="UT">Utah</option> 
+                            <option value="VT">Vermont</option> 
+                            <option value="VA">Virginia</option> 
+                            <option value="WA">Washington</option> 
+                            <option value="WV">West Virginia</option> 
+                            <option value="WI">Wisconsin</option> 
+                            <option value="WY">Wyoming</option>
+                            </select>
                         </div>
                     </div>
+
+
                     <div class="form-group">
                         <label for="" class="control-label col-xs-4">Zip</label>
                         <div class="col-xs-8">
                             <input type="text" class="form-control" name="zip" value="<?php echo $zip; ?>"/>
                         </div>
                     </div>
-
                     <input type="hidden" name="total" value="<?php echo $total; ?>"/>
                     <br/>
         
@@ -211,106 +266,8 @@ $total = 0;
                     </div>
 	        </form>
     <br/><br/><br/><br/>
-=======
-        <form class="form-group" method="POST" action="">
-		<h2>Credit Card Information</h2>
-		Card Type <br/>
-		<select name="type">
-			<option>Visa</option>
-			<option>Mastercard</option>
-		</select> <br/>
-		Name on the card <br/>
-		<input type="text" name="name" value="<?php echo $cardName; ?>"/><br/>
-		Card Number <br/>
-		<input type="text" name="number" value="<?php echo $cardNumber; ?>"/> <br/>
-		Security Code <br/>
-		<input type="text" name="security" value="<?php echo $security; ?>"/> <br/>
-		Expiration Date <br/>
-		<input type="text" name="expDate" value="<?php echo $expiration; ?>"/> <br/>
-		<br/>
-        <input type="radio" name="shipping" value="ship" checked="true"/>Ship to Address &nbsp; &nbsp; &nbsp; 
-        <input type="radio" name="shipping" value="pickup" />Pickup in Store <br/>
-        <div id="shippingInput">
-		<h2>Shipping Information </h2> <br/>
-		Street <br/>
-		<input type="text" name="street" value="<?php echo $street; ?>"/> <br/>
-		Street 2 <br/>
-		<input type="text" name="street2" value="<?php echo $street2; ?>"/> <br/>
-		City <br/>
-		<input type="text" name="city" value="<?php echo $city; ?>"/> <br/>
-		State <br/>
-		<select name="state"> 
-		<option value="" selected="selected">Select a State</option> 
-		<option value="AL">Alabama</option> 
-		<option value="AK">Alaska</option> 
-		<option value="AZ">Arizona</option> 
-		<option value="AR">Arkansas</option> 
-		<option value="CA">California</option> 
-		<option value="CO">Colorado</option> 
-		<option value="CT">Connecticut</option> 
-		<option value="DE">Delaware</option> 
-		<option value="DC">District Of Columbia</option> 
-		<option value="FL">Florida</option> 
-		<option value="GA">Georgia</option> 
-		<option value="HI">Hawaii</option> 
-		<option value="ID">Idaho</option> 
-		<option value="IL">Illinois</option> 
-		<option value="IN">Indiana</option> 
-		<option value="IA">Iowa</option> 
-		<option value="KS">Kansas</option> 
-		<option value="KY">Kentucky</option> 
-		<option value="LA">Louisiana</option> 
-		<option value="ME">Maine</option> 
-		<option value="MD">Maryland</option> 
-		<option value="MA">Massachusetts</option> 
-		<option value="MI">Michigan</option> 
-		<option value="MN">Minnesota</option> 
-		<option value="MS">Mississippi</option> 
-		<option value="MO">Missouri</option> 
-		<option value="MT">Montana</option> 
-		<option value="NE">Nebraska</option> 
-		<option value="NV">Nevada</option> 
-		<option value="NH">New Hampshire</option> 
-		<option value="NJ">New Jersey</option> 
-		<option value="NM">New Mexico</option> 
-		<option value="NY">New York</option> 
-		<option value="NC">North Carolina</option> 
-		<option value="ND">North Dakota</option> 
-		<option value="OH">Ohio</option> 
-		<option value="OK">Oklahoma</option> 
-		<option value="OR">Oregon</option> 
-		<option value="PA">Pennsylvania</option> 
-		<option value="RI">Rhode Island</option> 
-		<option value="SC">South Carolina</option> 
-		<option value="SD">South Dakota</option> 
-		<option value="TN">Tennessee</option> 
-		<option value="TX">Texas</option> 
-		<option value="UT">Utah</option> 
-		<option value="VT">Vermont</option> 
-		<option value="VA">Virginia</option> 
-		<option value="WA">Washington</option> 
-		<option value="WV">West Virginia</option> 
-		<option value="WI">Wisconsin</option> 
-		<option value="WY">Wyoming</option>
-		</select>
-		<br/>
-		Zip <br/>
-		<input type="text" name="zip" value="<?php echo $zip; ?>"/> <br/>
-		
-        </div>
-        <input type="hidden" name="total" value="<?php echo $total; ?>"/>
-        <br/>
-		<button name="checkoutInput" type="submit" class="btn btn-default">Continue Checkout</button>
-	</form>
-	
-	
-	
-	
->>>>>>> FETCH_HEAD
-    
-    </div>
-    
-</div>    
+
+       
 
 
 
