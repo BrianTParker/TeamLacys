3e4bbb875cec4c18188e9f49aeb87a64d53dcc4f
uptownjimmy <lljj@mac.com>
2014-03-20 22:15:24 -0400

    Details styling

--- details.php
+++ details.php
@@ -39,41 +39,47 @@ $ACCT_MGR = AccountManager::getInstance();
 
 
 	echo '<div class="row">' . "\n";
-
 	echo '<div class="col-sm-3 col-sm-offset-1">' . "\n";
-		echo '<h1>Reviews</h1>' . "\n";
+
+echo '<div class="panel panel-default">' . "\n";
+  echo '<div class="panel-heading">' . "\n";
+  echo '<class="panel-title"><h3>Reviews</h3>' . "\n";
+  echo '</div>' . "\n";
+
 		if($review_sql->rowCount() >0){
 			echo '<table class="table">' . "\n";
 			while($row = $review_sql->fetch()) {
 				echo '<tr>' . "\n";
 				echo '<td>' . "\n";
-				echo '<p>' . $row['review'] . '</p> <br/>';
-				echo '- ' . $row['first_name'] . ' ' . $row['last_name'];
+				echo '<p><i>' . $row['review'] . '</p> </i><br/>';
+				echo '- <strong>' . $row['first_name'] . ' ' . $row['last_name'] . ' </strong>';
 				echo '</td>' . "\n";
 				echo '</tr>' . "\n";
 				
 			}
 			echo '</table>' . "\n";
 			echo '<br/>';
-			echo '<h3>Write a review</h3/>';
+			echo '<h4></h4/>';
 		}else{
 			echo '<h3>Be the first to write a review</h3>';
 		}
-		
+				
+echo '<div class="panel-footer">' . "\n";
 		if($ACCT_MGR->isLoggedIn()){
 			echo '<form class="form-group" method="POST" action="">' . "\n";
-			echo '<textarea name ="review" class="form-control" rows="3"></textarea>';
+			echo '<textarea name ="review" placeholder="Write a Review" class="form-control" rows="3"></textarea>';
 			echo '<input type="hidden" name="customer_id" value="' . $ACCT_MGR->getId() . '"/>' . "\n";
 			echo '<input type="hidden" name="product_id" value="' . $productId . '"/>' . "\n";
-			echo '<button type="submit" class="btn btn-default">Post Review</button>' . "\n";
+			echo '<br/>';
+			echo '<p style="text-align:center;"><button type="submit"  class="btn btn-primary btn-xs">Post Review</button></p>' . "\n";
 			
 			echo '</form>';
 		}else{
 			echo '<a href="login.php"> Log in</a> to write a review';
 		}
-		
+		echo '</div>' . "\n";
 	echo '</div>' . "\n";
-
+echo '</div>' . "\n";
 
 	$details_sql = $DBH->query("select p.id as product_id, p.name as product_name, p.description as product_desc, p.image_location, p.price, pr.percentage, pr.expiration_date
 								from products p
@@ -84,18 +90,16 @@ $ACCT_MGR = AccountManager::getInstance();
 
 
 
-
-	$details_sql->setFetchMode(PDO::FETCH_ASSOC);
+$details_sql->setFetchMode(PDO::FETCH_ASSOC);
 	$row = $details_sql->fetch();
-	echo '<table class="table">';
+echo '<td><h3>' . $row['product_name'] . '</h3></td>' . "\n";
+	echo '<table class="table" width="200">';
+
 	echo '<tr>';
-	echo '<td><a href="./details.php?data=' . $row['product_id'] . '"><img src="' . $row['image_location'] . '"></a></td>' . "\n";
+	
 	echo '</tr>';
 	echo '<tr>';
-	echo '<td><h3>' . $row['product_name'] . '</h3></td>' . "\n";
-	echo '</tr>';
-	echo '<tr>';
-	echo '<td>' . $row['product_desc'] . '</td>' . "\n";
+	echo '<td style="text-align:center;"><h5>' . $row['product_desc'] . '</h5></td>' . "\n";
 	echo '<tr>';
 	if(isset($row['percentage']) ){
 		
@@ -108,37 +112,40 @@ $ACCT_MGR = AccountManager::getInstance();
 	if($promotion){
 		echo '<td>$' . number_format($promotional_price, 2) . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   <strong>' . $row['percentage'] * 100 . '% off!</strong></td>' . "\n"; 
 	}else{
-		echo '<td>$' . $row['price'] . '</td>' . "\n";
+		echo '<td style="text-align:center;"><h4>$' . $row['price'] . '</h4></td>' . "\n";
 	}
 	
 	echo '</tr>';
 
-	echo '<td>';
+	echo '<td style="text-align:center;">';
+	echo '<form class="form-inline">';
 	echo '<form id="addCartForm" method="POST" class="form">'; 
-	echo '<select name="quantity">';
+	echo '<label>Qty</label>';
+	echo '<br/>';
+	echo '<select style="width:50px" class="form-control" name="quantity">';
 	echo '<option>1</option>';
 	echo '<option>2</option>';
 	echo '<option>3</option>';
 	echo '<option>4</option>';
 	echo '<option>5</option>';
 	echo '</select>';
-	echo ' Qty';
+	echo '<br/>';echo '<br/>';
+	echo '<label>Size</label>';
 	echo '<br/>';
-	echo '<br/>';
-	echo '<select name="size">';
+	echo '<select style="width:50px" class="form-control" name="size">';
 	echo '<option>S</option>';
 	echo '<option>M</option>';
 	echo '<option>L</option>';
 	echo '<option>XL</option>';
 	echo '<option>XXL</option>';
 	echo '</select>';
-	echo ' Size';
 	echo '<br/>';
 	echo '<br/>';
+	echo '</form>';
 	
 	if ($ACCT_MGR->isLoggedIn()){
 	
-		echo '<button type="submit" class="btn btn-default">Add to cart</button>';
+		echo '<p style="text-align:center;"><button type="submit" class="btn btn-default">Add to cart</button></p>';
 		
 	}else{
 	
@@ -157,13 +164,17 @@ $ACCT_MGR = AccountManager::getInstance();
 	}
 	
 	echo '</form>' . "\n";
+
 	echo '</table>';
-		
+
+
 	
 	?>
-   
     </div>
-    
+
+    <?php
+    echo '<td><a href="./details.php?data=' . $row['product_id'] . '"><img src="' . $row['image_location'] . '" ></a></td>' . "\n";
+    ?>
 </div>
 <br/><br/><br/><br/>
 <?php
