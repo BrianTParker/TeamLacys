c303c8811c8f2bc0887d5b8f2b04a77deca1b98f
MisterToby <brithom.parker@gmail.com>
2014-03-23 11:27:03 -0400

    Refactored the details page to reduce the amount of php

--- details.php
+++ details.php
@@ -36,133 +36,140 @@ $ACCT_MGR = AccountManager::getInstance();
 								join customers c on c.id = r.customer_id
 								where r.product_id = " . $productId);
 								
+?>
 
+	<div class="row">
+		<div class="col-sm-3 col-sm-offset-1">
 
-	echo '<div class="row">' . "\n";
-	echo '<div class="col-sm-3 col-sm-offset-1">' . "\n";
+			<div class="panel panel-default" style="text-align:center;">
+				<div class="panel-heading">
+					<class="panel-title"><h3>Reviews</h3>
+				</div> 
+				<?php
+				if($review_sql->rowCount() >0){
+					echo '<table class="table">' . "\n";
+					while($row = $review_sql->fetch()) {
+						echo '<tr>' . "\n";
+						echo '<td>' . "\n";
+						echo '<p><i>' . $row['review'] . '</p> </i><br/>';
+						echo '- <strong>' . $row['first_name'] . ' ' . $row['last_name'] . ' </strong>';
+						echo '</td>' . "\n";
+						echo '</tr>' . "\n";
+						
+					}
+					echo '</table>' . "\n";
+					echo '<br/>';
+					echo '<h4></h4/>';
+				}else{
+					echo '<p><i><h5>(Be the first to write a review!)</h5></p> </i>';
+				}
+				?>
 
-echo '<div class="panel panel-default" style="text-align:center;">' . "\n";
-  echo '<div class="panel-heading">' . "\n";
-  echo '<class="panel-title"><h3>Reviews</h3>' . "\n";
-  echo '</div>' . "\n";
-
-		if($review_sql->rowCount() >0){
-			echo '<table class="table">' . "\n";
-			while($row = $review_sql->fetch()) {
-				echo '<tr>' . "\n";
-				echo '<td>' . "\n";
-				echo '<p><i>' . $row['review'] . '</p> </i><br/>';
-				echo '- <strong>' . $row['first_name'] . ' ' . $row['last_name'] . ' </strong>';
-				echo '</td>' . "\n";
-				echo '</tr>' . "\n";
+			<div class="panel-footer">
+			<?php
+			if($ACCT_MGR->isLoggedIn()){
+				echo '<form class="form-group" method="POST" action="">' . "\n";
+				echo '<textarea name ="review" placeholder="Write a Review" class="form-control" rows="3"></textarea>';
+				echo '<input type="hidden" name="customer_id" value="' . $ACCT_MGR->getId() . '"/>' . "\n";
+				echo '<input type="hidden" name="product_id" value="' . $productId . '"/>' . "\n";
+				echo '<br/>';
+				echo '<p style="text-align:center;"><button type="submit"  class="btn btn-primary btn-xs">Post Review</button></p>' . "\n";
 				
+				echo '</form>';
+			}else{
+				echo '<a href="login.php"> Log in</a> to write a review';
 			}
-			echo '</table>' . "\n";
-			echo '<br/>';
-			echo '<h4></h4/>';
-		}else{
-			echo '<p><i><h5>(Be the first to write a review!)</h5></p> </i>';
-		}
-				
-echo '<div class="panel-footer">' . "\n";
-		if($ACCT_MGR->isLoggedIn()){
-			echo '<form class="form-group" method="POST" action="">' . "\n";
-			echo '<textarea name ="review" placeholder="Write a Review" class="form-control" rows="3"></textarea>';
-			echo '<input type="hidden" name="customer_id" value="' . $ACCT_MGR->getId() . '"/>' . "\n";
-			echo '<input type="hidden" name="product_id" value="' . $productId . '"/>' . "\n";
-			echo '<br/>';
-			echo '<p style="text-align:center;"><button type="submit"  class="btn btn-primary btn-xs">Post Review</button></p>' . "\n";
-			
-			echo '</form>';
-		}else{
-			echo '<a href="login.php"> Log in</a> to write a review';
-		}
-		echo '</div>' . "\n";
-	echo '</div>' . "\n";
-echo '</div>' . "\n";
-
+			?>
+			</div>
+		</div>
+</div>
+	
+	<?php
 	$details_sql = $DBH->query("select p.id as product_id, p.name as product_name, p.description as product_desc, p.image_location, p.price, pr.percentage, pr.expiration_date
 								from products p
 								left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()							
 								where p.id = " . $productId);
-	echo '<div class="col-sm-2 col-sm-offset-1">' . "\n";
+	?>
+	<div class="col-sm-2 col-sm-offset-1">
 
 
 
+	<?php
+	$details_sql->setFetchMode(PDO::FETCH_ASSOC);
+		$row = $details_sql->fetch();
+	echo '<td><h3>' . $row['product_name'] . '</h3></td>' . "\n";
+		echo '<table class="table" width="200">';
 
-$details_sql->setFetchMode(PDO::FETCH_ASSOC);
-	$row = $details_sql->fetch();
-echo '<td><h3>' . $row['product_name'] . '</h3></td>' . "\n";
-	echo '<table class="table" width="200">';
-
-	echo '<tr>';
-	
-	echo '</tr>';
-	echo '<tr>';
-	echo '<td style="text-align:center;"><h5>' . $row['product_desc'] . '</h5></td>' . "\n";
-	echo '<tr>';
-	if(isset($row['percentage']) ){
+		echo '<tr>';
 		
-		$promotion = true;
-		$promotional_price = ($row['price'] - ($row['price'] * $row['percentage']));
-		$exp_date = strtotime($row['expiration_date']);
-		
-		
-	}
-	if($promotion){
-		echo '<td>$' . number_format($promotional_price, 2) . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   <strong>' . $row['percentage'] * 100 . '% off!</strong></td>' . "\n"; 
-	}else{
-		echo '<td style="text-align:center;"><h4>$' . $row['price'] . '</h4></td>' . "\n";
-	}
-	
-	echo '</tr>';
+		echo '</tr>';
+		echo '<tr>';
+		echo '<td style="text-align:center;"><h5>' . $row['product_desc'] . '</h5></td>' . "\n";
+		echo '<tr>';
+		if(isset($row['percentage']) ){
+			
+			$promotion = true;
+			$promotional_price = ($row['price'] - ($row['price'] * $row['percentage']));
+			$exp_date = strtotime($row['expiration_date']);
+			
+			
+		}
+		if($promotion){
+			echo '<td>$' . number_format($promotional_price, 2) . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   <strong>' . $row['percentage'] * 100 . '% off!</strong></td>' . "\n"; 
+		}else{
+			echo '<td style="text-align:center;"><h4>$' . $row['price'] . '</h4></td>' . "\n";
+		}
+	?>
+	</tr>
 
-	echo '<td style="text-align:center;">';
-	//echo '<form class="form-inline">';
-	echo '<form id="addCartForm" method="POST" class="form-inline">'; 
-	echo '<label>Qty</label>';
-	echo '<br/>';
-	echo '<select style="width:60px" class="form-control" name="quantity">';
-	echo '<option>1</option>';
-	echo '<option>2</option>';
-	echo '<option>3</option>';
-	echo '<option>4</option>';
-	echo '<option>5</option>';
-	echo '</select>';
-	echo '<br/>';echo '<br/>';
-	echo '<label>Size</label>';
-	echo '<br/>';
-	echo '<select style="width:60px" class="form-control" name="size">';
-	echo '<option>S</option>';
-	echo '<option>M</option>';
-	echo '<option>L</option>';
-	echo '<option>XL</option>';
-	echo '<option>XXL</option>';
-	echo '</select>';
-	echo '<br/>';
-	echo '<br/>';
+	<td style="text-align:center;">
+	<?php //echo '<form class="form-inline">'; ?>
+	<form id="addCartForm" method="POST" class="form-inline">
+	<label>Qty</label>
+	<br/>
+	<select style="width:60px" class="form-control" name="quantity">
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
+	<option>4</option>
+	<option>5</option>
+	</select>
+	<br/><br/>
+	<label>Size</label>
+	<br/>
+	<select style="width:60px" class="form-control" name="size">
+	<option>S</option>
+	<option>M</option>
+	<option>L</option>
+	<option>XL</option>
+	<option>XXL</option>
+	</select>
+	<br/>
+	<br/>
 	
 	
-	echo '<p style="text-align:center;"><button type="submit" class="btn btn-default">Add to cart</button></p>';
+	<p style="text-align:center;"><button type="submit" class="btn btn-default">Add to cart</button></p>
 	
-	echo '<input type="hidden" name="id" value="' . $row['product_id'] . '"/>';
-	echo '<input type="hidden" name="name" value="' . $row['product_name'] . '"/>';
-	echo '<input type="hidden" name="description" value="' . $row['product_desc'] . '"/>';
-	echo '<input type="hidden" name="image_location" value="' . $row['image_location'] . '"/>';
+	<input type="hidden" name="id" value="<?php echo $row['product_id']; ?>"/>
+	<input type="hidden" name="name" value="<?php echo $row['product_name']; ?>"/>
+	<input type="hidden" name="description" value="<?php echo $row['product_desc']; ?>"/>
+	<input type="hidden" name="image_location" value="<?php echo $row['image_location']; ?>"/>
+	<?php
 	if($promotion){
 		echo '<input type="hidden" name="price" value="' . $promotional_price . '"/>';
 	}else{
 		echo '<input type="hidden" name="price" value="' . $row['price'] . '"/>';
 	}
-	
-	echo '</form>' . "\n";
-	//echo '</form>';
-
-	echo '</table>';
-
-
-	
 	?>
+	
+	</form>
+	</form>
+
+	</table>
+
+
+	
+	
     </div>
 
     <?php
