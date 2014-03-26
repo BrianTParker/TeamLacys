11c9622052cfa4bcd841377940b1140f253b25fc
nmalacarne <nicholas.malacarne@gmail.com>
2014-03-26 02:06:51 -0400

    More Tweaks #93
    - './guestcheckout.php' now accepts the order total as POST data under
    the key 'data'
    - './guestcheckout.php' now stores POST data under key 'data' into a
    hidden field within the 'guestUserForm' HTML form

--- guestcheckout.php
+++ guestcheckout.php
@@ -33,6 +33,7 @@ include "header.php";
 		or
 		<a href="./login.php">Login</a>/<a href="./newuser.php">Register</a>
 		
+		<input type="hidden" name="total" value="<?php echo $_POST["total"]; ?>"/>
 	</form>
 	
 </div>
