7c1130e5f4d97b700ab3028e2d085d819ac2e6f0
nmalacarne <nicholas.malacarne@gmail.com>
2014-03-25 20:00:07 -0400

    Closes #86
    - added an HTML form with id equal to 'guestUserForm' to file
    './guestcheckout.php'
    - 'guestUserForm' now asks the user for their first name, last name,
    email address, and phone number
    - currently, the 'continue checkout' submit button just reloads the page
    - added links to './login.php' and './newuser.php' next to the 'contine
    checkout' submit button

--- guestcheckout.php
+++ guestcheckout.php
@@ -1,12 +1,41 @@
 <?php
 // RUN: http://localhost/TeamLacys/guestcheckout.php
-?>
-<?php
+?><?php
 include "header.php";
 ?>
-<?php 
-echo "TODO: write content";
-?>
+<div class="col-xs-6 col-sm-6 col-md-2 col-lg-4 col-sm-offset-3 col-md-offset-5 col-lg-offset-4">
+
+	<h1>Guest Checkout</h1>
+	
+	<form id="guestUserForm" role="form">
+	
+		<div class="form-group ">
+			<label for="guestFName">First Name</label>
+			<input type="text" class="form-control" id="guestFName" placeholder="Enter First Name" required>
+		</div>
+		
+		<div class="form-group">
+			<label for="guestLName">Last Name</label>
+			<input type="text" class="form-control" id="guestLName" placeholder="Enter Last Name" required>
+		</div>
+		
+		<div class="form-group">
+			<label for="guestEmail">Email</label>
+			<input type="email" class="form-control" id="guestEmail" placeholder="Enter Email" required>
+		</div>
+		
+		<div class="form-group">
+			<label for="guestPhone">Phone Number</label>
+			<input type="tel" class="form-control" id="guestPhone" placeholder="Enter Phone Number" required>
+		</div>
+		
+		<button type="submit" class="btn btn-success">Continue Checkout</button>
+		or
+		<a href="./login.php">Login</a>/<a href="./newuser.php">Register</a>
+		
+	</form>
+	
+</div>
 <?php
 include "footer.php"
 ?>
