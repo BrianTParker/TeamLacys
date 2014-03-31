b3940e448f0ef86f371cc0732d48bf8562294ffe
dvaidya <dhwanivaidya09@gmail.com>
2014-03-31 17:42:03 -0400

    Updated mens.php
    updated mens.php

--- mens.php
+++ mens.php
@@ -1,3 +1,4 @@
+
 <?php
 include "header.php";
 ?>
@@ -5,7 +6,7 @@ include "header.php";
 	<div class="row">
 	   <div class="col-sm-8 col-sm-offset-2">
 	   
-			<div class = text-center>
+			<div class ="text-center">
 				<!--This is generic image area which has image of men's generic products-->	
 				<img src = "img/img3.jpg" alt="Generic men's product" width= "850" height="150">
 				</br></br>
@@ -20,15 +21,32 @@ include "header.php";
 									left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
 									where p.age_category = 'adult' and p.gender_category = 'male' and p.article_category = 'pants'");								
 					$pants_count_sql = $DBH->query("select count(*) from products where article_category = 'pants'");
-					$shirts_sql = $DBH->query("select id, name, description, image_location,price from products where age_category = 'adult' and gender_category = 'male' and article_category = 'shirts'");
+
+
+					$shirts_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage,pr.expiration_date
+									from products p
+									left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+									where p.age_category = 'adult' and p.gender_category = 'male' and p.article_category = 'shirts'");	
 					$shirts_count_sql = $DBH->query("select count(*) from products where article_category = 'shirts'");
-					$belts_sql = $DBH->query("select id, name, description, image_location,price from products where age_category = 'adult' and gender_category = 'male' and article_category = 'belts'");
+
+
+					$belts_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage,pr.expiration_date
+									from products p
+									left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+									where p.age_category = 'adult' and p.gender_category = 'male' and p.article_category = 'belts'");
 					$belts_count_sql = $DBH->query("select count(*) from products where article_category = 'belts'");
-					$watches_sql = $DBH->query("select id, name, description, image_location,price from products where age_category = 'adult' and gender_category = 'male' and article_category = 'watches'");
+
+
+					$watches_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage,pr.expiration_date
+									from products p
+									left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+									where p.age_category = 'adult' and p.gender_category = 'male' and p.article_category = 'watches'");
+
+
 					$watches_count_sql = $DBH->query("select count(*) from products where article_category = 'watches'");
-					$per_row = 3;
-					$promotion = false;
+					$per_row = 4;
 					?>
+					<!-- navigation bar -->
 					<li class="active"><a href="#Pants" data-toggle="tab">Pants</a></li>
 					
 					<li><a href="#Shirts" data-toggle="tab">Shirts</a></li>
@@ -37,26 +55,29 @@ include "header.php";
 					
 					<li><a href="#Watches" data-toggle="tab">Watches</a></li>
 								
-				</ul>
+				</ul><!-- end of navigation bar -->
 					
-				<div class="tab-content">
-					<div class="tab-pane active text-center" id="Pants">
+				<div class="tab-content" >
+					<div class="tab-pane active" id="Pants"> <!-- pants tab -->
 					
-						<table class="table">
+						<table class = "table table-product">
 						<?php
 						$pants_sql->setFetchMode(PDO::FETCH_ASSOC);
 						$i = 0;
 						$count1 = $pants_count_sql->fetch();							
-						
-						while($row = $pants_sql->fetch()) {
+						echo "<br/>\n";
+
+
+						while($row = $pants_sql->fetch()) {	
 							$promotion = false;
 							# get the image and description of product in one cell
 							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . 
-										'"width=200 height=250></a>';
+										'"width=192 height=240 class="img-thumbnail"></a>';
 							echo "<br/><br/>\n";
 							echo '<a href="./details.php?data=' . $row['id'] . '">' . $row['name'] . '</a>';
 							echo "<br/>\n";		
-																					
+
+
 							if(isset($row['percentage']) )
 							{
 								$promotion = true;
@@ -65,115 +86,176 @@ include "header.php";
 							}
 							if($promotion)
 							{
-								echo 'Reg. $ '. $row['price'];	
+								echo '<font color = "gray">Reg. $'. $row['price'];	
 								echo "</br>\n";
-								echo '<font color = "red">Sale $' . number_format($promotional_price, 2) ;
+								echo '<font color = "red"><strong>Sale $' . number_format($promotional_price, 2) ;
 							}
 							else
 							{
-								echo '$ '. $row['price'];	
+								echo '<strong>$'. $row['price'];	
 							}
 							echo "<br/><br/>\n";
 							echo '</td>';
-																					
-							# display pants 4 columns
+
+
+							# display pants in 3 columns
 							if (++$i % $per_row == 0 && $i >0 && $i < $count1) {
-							echo '</tr><tr>';
+							echo '<tr></tr>';
 							}
-						}
-						# If the last row isn't 'full', fill it with empty cells
-						for($x1 = 0; $x1 < $per_row - $i % $per_row; $x1++) {
-							echo '<td></td>';
 						}
 						?>                        
 						</table>
 					</div> <!--end of pants tab-->
 					
 					<!-- shirts tab-->
-					<div class="tab-pane text-center"" id="Shirts"> 
-						<table class="table">
+					<div class="tab-pane" id="Shirts"> 
+						<table class="table table-product">
 						<?php
 						$shirts_sql->setFetchMode(PDO::FETCH_ASSOC);
 						$j = 0;
 						$count2 = $shirts_count_sql->fetch();
-						
-						
+
+
+						echo "<br/>\n";
 						while($row = $shirts_sql->fetch()) {
+							$promotion = false;
 							# get the image and description of product in one cell
-							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . 
-										'"width=192 height=240></a>';
+							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . '"
+											width=192 height=240 class="img-thumbnail"></a>';
 							echo "<br/><br/>\n";
 							echo '<a href="./details.php?data=' . $row['id'] . '">' . $row['name'] . '</a>';
+							echo "<br/>\n";
+
+
+							if(isset($row['percentage']) )
+							{
+								$promotion = true;
+								$promotional_price = ($row['price'] - ($row['price'] * $row['percentage']));
+								$exp_date = strtotime($row['expiration_date']);
+							}
+							if($promotion)
+							{
+								echo '<font color = "gray">Reg. $'. $row['price'];	
+								echo "</br>\n";
+								echo '<font color = "red"><strong>Sale $' . number_format($promotional_price, 2) ;
+							}
+							else
+							{
+								echo '<strong>$'. $row['price'];	
+							}
 							echo "<br/><br/>\n";
 							echo '</td>';
+
+
 							# display shirts in 3 columns
 							if (++$j % $per_row == 0 && $j >0 && $j < $count2) {
-							echo '</tr><tr>';
+							echo '<tr></tr>';
 							}
-						}
-						# If the last row isn't 'full', fill it with empty cells
-						for($x2 = 0; $x2 < $per_row - $j % $per_row; $x2++) {
-							echo '<td></td>';
 						}
 						?>                        
 						</table>
 					</div><!-- end of shirts tab-->
 					
 					<!-- Belts tab-->
-					<div class="tab-pane text-center"" id="Belts">
-						<table class="table">
+					<div class="tab-pane" id="Belts">
+						<table class="table table-product">
 						<?php
 						$belts_sql->setFetchMode(PDO::FETCH_ASSOC);
 						$k = 0;
 						$count3 = $belts_count_sql->fetch();
-						
+
+
+						echo "<br/>\n";
 						while($row = $belts_sql->fetch()) {
+
+
+							$promotion = false;
 							# get the image and description of product in one cell
-							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . 
-							'"width=192 height=240></a>';
+							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . '"
+												class="img-thumbnail"></a>';
 							echo "<br/><br/>\n";
 							echo '<a href="./details.php?data=' . $row['id'] . '">' . $row['name'] . '</a>';
+							echo "<br/>\n";
+
+
+							if(isset($row['percentage']) )
+							{
+								$promotion = true;
+								$promotional_price = ($row['price'] - ($row['price'] * $row['percentage']));
+								$exp_date = strtotime($row['expiration_date']);
+							}
+							if($promotion)
+							{
+								echo '<font color = "gray">Reg. $'. $row['price'];	
+								echo "</br>\n";
+								echo '<font color = "red"><strong>Sale $' . number_format($promotional_price, 2) ;
+							}
+							else
+							{
+								echo '<strong>$'. $row['price'];	
+							}
 							echo "<br/><br/>\n";
 							echo '</td>';
+
+
 							# display belts in 3 columns
 							if (++$k % $per_row == 0 && $k >0 && $k < $count3) {
 							echo '</tr><tr>';
 							}
 						}
-						# If the last row isn't 'full', fill it with empty cells
-						for($x3 = 0; $x3 < $per_row - $k % $per_row; $x3++) {
-							echo '<td></td>';
-						}
+
+
 						?>                        
 						</table>
 					</div> <!-- end of belts tab-->
 					
 					<!-- Watches tab-->
-					<div class="tab-pane text-center"" id="Watches">
-						<table class="table">
+					<div class="tab-pane " id="Watches">
+						<table class="table table-product">
 						<?php
 						$watches_sql->setFetchMode(PDO::FETCH_ASSOC);
 						$l = 0;
 						$count4 = $watches_count_sql->fetch();
-						
+
+
+						echo "<br/>\n";
 						while($row = $watches_sql->fetch()) {
+							$promotion = false;
 							# get the image and description of product in one cell
-							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . '"></a>';
+							echo '<td><a href="./details.php?data=' . $row['id'] . '"><img src="' . $row['image_location'] . '" 
+											class="img-thumbnail"></a>';
 							echo "<br/><br/>\n";
 							echo '<a href="./details.php?data=' . $row['id'] . '">' . $row['name'] . '</a>';
+							echo "<br/>\n";
+
+
+							if(isset($row['percentage']) )
+							{
+								$promotion = true;
+								$promotional_price = ($row['price'] - ($row['price'] * $row['percentage']));
+								$exp_date = strtotime($row['expiration_date']);
+							}
+							if($promotion)
+							{
+								echo '<font color = "gray">Reg. $'. $row['price'];	
+								echo "</br>\n";
+								echo '<font color = "red"><strong>Sale $' . number_format($promotional_price, 2) ;
+							}
+							else
+							{
+								echo '<strong>$'. $row['price'];	
+							}
 							echo "<br/><br/>\n";
 							echo '</td>';
+
+
 							# display watches in 3 columns
 							if (++$l % $per_row == 0 && $l >0 && $l < $count4) {
 							echo '</tr><tr>';
 							}
 						}
-						# If the last row isn't 'full', fill it with empty cells
-						for($x4 = 0; $x4 < $per_row - $l % $per_row; $x4++) {
-							echo '<td></td>';
-						}
-						?>                        
-						</table>
+					?>                        
+					</table>
 					</div> <!-- end of watches tab-->
 				</div><!-- end of tab-content-->
 				
@@ -182,6 +264,7 @@ include "header.php";
 	</div> <!-- end of row-->
 </div> <!-- end of container-fluid-->
 <br/><br/><br/><br/>
+
 
 <?php
 include "footer.php"
