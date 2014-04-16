bad931190ed73286446e309f3bffc6401d2e307d
asmaliza96 <as_hadi96@yahoo.com>
2014-04-15 21:21:05 -0400

    Complete sales items page
    include new banner, beauty, home essentials, and kitchen

--- salesItem.php
+++ salesItem.php
@@ -35,6 +35,31 @@ include_once "Products/products.php";
 								
 	$sale_child_count_sql = $DBH->query("select count(*) from products p left join promotions pr on pr.product_id = p.id where p.age_category = 'child' and pr.percentage !=0");
 	
+	$kitchen_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage, pr.expiration_date
+								from products p
+								left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+								where p.product_category = 'bakeware' or p.product_category = 'cookwear' or p.product_category = 'dining' or p.product_category = 'smallappliances'");
+								
+	$sale_kitchen_count_sql = $DBH->query("select count(*) from products p left join promotions pr on pr.product_id = p.id where 
+								p.product_category = 'bakeware' or p.product_category = 'cookwear' or p.product_category = 'dining' or p.product_category = 'smallappliances' and pr.percentage !=0");
+	
+	
+	$homeEssentials_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage, pr.expiration_date
+								from products p
+								left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+								where p.product_category = 'bedBath' or p.product_category = 'comforters' or p.product_category = 'luggage'");
+								
+	$sale_homeEssentials_count_sql = $DBH->query("select count(*) from products p left join promotions pr on pr.product_id = p.id where 
+								p.product_category = 'bedBath' or p.product_category = 'comforters' or p.product_category = 'luggage' and pr.percentage !=0");
+	
+	
+	$beauty_sql = $DBH->query("select p.id, p.name, p.description, p.image_location, p.price, pr.percentage, pr.expiration_date
+								from products p
+								left join promotions pr on pr.product_id = p.id and pr.expiration_date >= CURDATE()	
+								where p.product_category = 'haircare' or p.product_category = 'perfume' or p.product_category = 'makeup' or p.product_category = 'cologne' ");
+								
+	$sale_beauty_count_sql = $DBH->query("select count(*) from products p left join promotions pr on pr.product_id = p.id where
+								p.product_category = 'haircare' or p.product_category = 'perfume' or p.product_category = 'makeup' or p.product_category = 'cologne' and pr.percentage !=0");
 	
 	$per_row = 4;
 ?>
@@ -45,7 +70,7 @@ include_once "Products/products.php";
 		<div class="col-sm-8 col-sm-offset-2">
 			<div class ="text-center">
 				<!--Image banner-->	
-					<img src = "img/bannerSale.jpg" alt="Generic men's product" width= "870" height="150">
+					<img src = "img/bannerSale2.png" alt="Generic men's product" width= "870" height="150">
 					<a href = "top"></a>
 					</br></br>
 			</div><!-- end of div for image -->
@@ -170,15 +195,60 @@ include_once "Products/products.php";
 					<!-- Kitchen tab -->
 					<div class="tab-pane" id="Kitchen"> 
 						<table class = "table table-product">
-					
+						<?php
+							$kitchen_sql->setFetchMode(PDO::FETCH_ASSOC);
+							$i = 0;
+							$count4 = $sale_kitchen_count_sql->fetch();							
+						
+							echo "<br/>\n";
+								
+							while($row = $kitchen_sql->fetch()) {	
+								if(isset($row['percentage']) )
+								{
+									echo '<td>';
+									echo displayProducts($row['id'], $row['image_location'], $row['name']);
+									echo displayPrice($row['price'], $row['percentage'], $row['expiration_date']);
+									echo '</td>';								
+
+									// display kitchen's in 4 columns
+									if (++$i % $per_row == 0 && $i >0 && $i < $count4) {
+										echo '<tr></tr>';
+									}
+								}
+
+							}
+						?> 
 				
 						</table>
 					
 					</div><!--end of div for kitchen's sales item-->
 			
 					<!-- Home tab -->
-					<div class="tab-pane active" id="HomeEssentials"> 
+					<div class="tab-pane" id="HomeEssentials"> 
 						<table class = "table table-product">
+						<?php
+							$homeEssentials_sql->setFetchMode(PDO::FETCH_ASSOC);
+							$i = 0;
+							$count5 = $sale_homeEssentials_count_sql->fetch();							
+						
+							echo "<br/>\n";
+								
+							while($row = $homeEssentials_sql->fetch()) {	
+								if(isset($row['percentage']) )
+								{
+									echo '<td>';
+									echo displayProducts($row['id'], $row['image_location'], $row['name']);
+									echo displayPrice($row['price'], $row['percentage'], $row['expiration_date']);
+									echo '</td>';								
+
+									// display children's in 4 columns
+									if (++$i % $per_row == 0 && $i >0 && $i < $count5) {
+										echo '<tr></tr>';
+									}
+								}
+
+							}
+						?> 
 					
 				
 						</table>
@@ -188,8 +258,29 @@ include_once "Products/products.php";
 					<!-- Beauty tab -->
 					<div class="tab-pane" id="Beauty"> 
 						<table class = "table table-product">
-					
-				
+						<?php
+							$beauty_sql->setFetchMode(PDO::FETCH_ASSOC);
+							$i = 0;
+							$count6 = $sale_beauty_count_sql->fetch();							
+						
+							echo "<br/>\n";
+								
+							while($row = $beauty_sql->fetch()) {	
+								if(isset($row['percentage']) )
+								{
+									echo '<td>';
+									echo displayProducts($row['id'], $row['image_location'], $row['name']);
+									echo displayPrice($row['price'], $row['percentage'], $row['expiration_date']);
+									echo '</td>';								
+
+									// display children's in 4 columns
+									if (++$i % $per_row == 0 && $i >0 && $i < $count6) {
+										echo '<tr></tr>';
+									}
+								}
+
+							}
+						?> 
 						</table>
 					
 					</div><!--end of div for kitchen's sales item-->
