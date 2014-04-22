597c175ceaba563e9a95b14d3047366f7afdf3ec
uptownjimmy <lljj@mac.com>
2014-04-22 19:52:26 -0400

    About

--- about.php
+++ about.php
@@ -5,7 +5,19 @@ include "header.php";
 <div class="row">
 
     <div class="col-sm-4 col-sm-offset-4">
-        Content goes here
+        John Lacy started his first store in Kansas City in 1910. 
+        He sold quality goods at affordable prices, and he 
+        made sure that every customer felt like they were the most 
+        important person in the store. <br/><br/>
+
+        Today, Lacy's is dedicated to ensuring that John's values and 
+        hard work live on. We are proud to serve you. <br/><br/>
+
+        If at any time you are dissatisfied, or if you have questions, 
+        please contact us.<br/><br/>
+
+        888-888-8888<br/>
+        <li><a href="contact.php">Contact Us</a></li>
     </div>
     
 </div>
