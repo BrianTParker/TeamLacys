9c89de2da208e55e77e2cf3bd1ed2e4cb5a33dc3
uptownjimmy <lljj@mac.com>
2014-04-22 20:00:54 -0400

    About yet again

--- about.php
+++ about.php
@@ -4,21 +4,30 @@ include "header.php";
 
 <div class="row">
 
-    <div class="col-sm-4 col-sm-offset-4">
-        John Lacy started his first store in Kansas City in 1910. 
+    <div class="row">
+  <div class="col-sm-4 col-sm-offset-4 text-center">
+	  <h4> 
+	  	John Lacy started his first store in Kansas City in 1910. 
         He sold quality goods at affordable prices, and he 
         made sure that every customer felt like they were the most 
-        important person in the store. <br/><br/>
-
-        Today, Lacy's is dedicated to ensuring that John's values and 
-        hard work live on. We are proud to serve you. <br/><br/>
-
-        If at any time you are dissatisfied, or if you have questions, 
-        please contact us.<br/><br/>
-
-        Phone: 888-888-8888<br/>
-        <li><a href="contact.php">Email</a></li>
-    </div>
+        important person in the store. 
+    </h4>
+	  <h4>
+	  	 Today, Lacy's is dedicated to ensuring that John's values and 
+        hard work live on. We are proud to serve you.  
+	  </h4>
+	  <h4>
+	  	If at any time you are dissatisfied, or if you have questions, 
+        please contact us. 
+    </h4>
+    <h4>
+	  	Phone: 888-888-8888
+    </h4>
+    <h4>
+	  	<a href="contact.php">Email</a>
+    </h4>
+  </div>
+</div>
     
 </div>
 
@@ -28,3 +37,4 @@ include "header.php";
 <?php
 include "footer.php";
 ?>
+
